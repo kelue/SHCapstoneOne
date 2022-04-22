08e098bafbddcc6bf651eb4d9c0837511a2df555
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-04-22 22:31:37 +0100

    Create: server.js Logics
    I created the server.js logic,
    
    i changed the pages folder to views because am using ejs to render my views pages.

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
-// to do 
-// nodejs server logic code goes here 
+const http = require('http');
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+
+const host = '127.0.0.1';
+const port = 3000;
+
+
+app.get('/', function(req, res) {
+    res.render('index');
+})
+
+app.get('/about', function(req, res) {
+    res.render('about');
+})
+
+app.get('*', function(req, res) {
+    res.render('404');
+})
+
+app.listen(port, function(err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", port);
+});
