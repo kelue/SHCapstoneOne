70091d7a2dbf42cecd3c4200fdf0d1ebc5352bc7
kelue <austincmon5@gmail.com>
2022-04-24 02:36:08 +0100

    finish up overall work

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const http = require('http');
 
 // file system module to perform file operations
 const fs = require('fs');
-const os = require('os');
 
+// import file with logic for generating os data
+const os = require('./getOsData')
 
+//declare host and port
 const host = '127.0.0.1';
 const port = 3000;
 
@@ -13,63 +15,35 @@ const server = http.createServer((req, res) => {
     const urlPath = req.url;
 
     if (urlPath === '/') {
-        res.statusCode = 200;
-        res.setHeader('Content-type', 'text/html');
+        res.writeHead(200, {'Content-Type': 'text/html'});
         html = fs.readFileSync('./pages/index.html');
         res.end(html);
     } else if (urlPath === '/about') {
-        res.statusCode = 200;
-        res.setHeader('Content-type', 'text/html');
+        res.writeHead(200, {'Content-Type': 'text/html'});
         html = fs.readFileSync('./pages/about.html');
         res.end(html);
     } else if (urlPath === '/sys') {
-        res.statusCode = 201;
-        res.setHeader('Content-type', 'text/plain');
-
-        // json data
-        const hostName = os.hostname();
-        const platform = os.platform();
-        const architecture = os.arch();
-        const numberOfCPUS = os.cpus();
-        const networkInterfaces = os.networkInterfaces();
-        const uptime = os.uptime();
-      
-        const jsonData = `{
-        "hostname": "${hostName}",
-        "platform": "${platform}",
-        "architecture": "${architecture}",
-        "numberOfCPUS": "${numberOfCPUS.length}",
-        "networkInterfaces": "${networkInterfaces}",
-        "uptime" : "${uptime}"
-        }`;
-
-        // parse json
-        const jsonObj = JSON.parse(jsonData);
-        console.log(jsonObj);
+        res.writeHead(201, {'Content-Type': 'text/plain'});
 
         // stringify JSON Object
-        var jsonContent = JSON.stringify(jsonObj);
-        console.log(jsonContent);
+        let jsonContent = JSON.stringify(os.jsonData, null, 2);
 
         fs.writeFile("osinfo.json", jsonContent, 'utf8', function(err) {
             if (err) {
-                console.log("An error occured while writing JSON Object to File.");
-                return console.log(err);
+                return console.log("An error occured", err);
             }
-
             console.log("JSON file has been saved.");
             res.end('Your OS info has been saved successfully!');
         });
 
       
     } else {
-        res.statusCode = 404;
-        res.setHeader('Content-type', 'text/html');
+        res.writeHead(404, {'Content-Type': 'text/html'});
         html = fs.readFileSync('./pages/404.html');
         res.end(html);
     }
 });
 
 server.listen(port, host, () => {
-    console.log(`Server running at ${ host }: ${ port }`);
+    console.log(`Server running at ${host}:${port}`);
 });
